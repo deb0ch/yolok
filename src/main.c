8ffacdcf8993344ed601ad366d2776a4a0ce17bc
Thomas de Beauchene <thomas.chauvot-de-beauchene@epitech.eu>
2015-10-31 17:38:24 +0100

    Added multiboot magic check

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <chauvo_t@epitech.net>
 **
 ** Started on  Fri May 22 18:33:17 2015 chauvo_t
-** Last update Fri Jul 24 17:03:15 2015 deb0ch
+** Last update Sat Oct 31 16:54:51 2015 chauvo_t
 */
 
 #include "kwrite.h"
@@ -14,11 +14,19 @@
 #include "utils.h"
 #include "vga.h"
 
-void    yolomain()
+#define MAGIC_MULTIBOOT	0x2badb002
+
+void    yolomain(unsigned int magic, const void *multiboot)
 {
+	(void)multiboot;
+	if (magic != MAGIC_MULTIBOOT)
+	{
+		kwrite(KWRITE_ALL, "error: wrong magic\n", 19);
+		return ;
+	}
 	vga_clr();
 	init_serial_port(COM1);
 	while (42)
-		kwrite(KWRITE_ALL, "Yoloooooooooooooooooooooooooooooooooooooooooooooooo !\n", 54);
+		kwrite(KWRITE_ALL, "Yoloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo !\n", 54);
 	init_flat_gdt();
 }
